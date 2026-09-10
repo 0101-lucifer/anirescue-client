41c0671b159fa2d37adc56e2665f0cd48ac21010
Sadiq Zakir Pathan <22co39@aiktc.ac.in>
2026-09-10 05:34:34 +0530

    Point login button to correct login endpoint

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
     const endpoint = isRegistering ? '/api/auth/register' : '/api/auth/login';
     
     try {
-      const response = await fetch('https://anirescue-api.onrender.com/api/auth/register', {
+      const response = await fetch('`https://anirescue-api.onrender.com${endpoint}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
